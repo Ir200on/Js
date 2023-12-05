d4c4c54a54704a805596c2c08adbeaf576e17f28
Ir200on <snezokdasha@gmail.com>
2023-12-05 23:44:26 +0200

    124

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// 1-task
+// let name = "Іван";
+// let city = "Іван";
+
+// console.log(city);  //Іван
+
+// 2-task
+// let name = "Olga";
+// console.log(`привіт ${1}`);  //Привіт 1
+// console.log(`привіт ${"name"}`);  //Привіт name
+// console.log(`привіт ${name}`);  //Привіт Olga
+
+// 3-task
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+// console.log(typeof(a));  //string
+// console.log(typeof(b));  //string
+// console.log(typeof(c));  //string
+
+// 4-task
+// console.log((0.1 * 10 + 0.2*10) / 10);  //0.3
+
+// 5-task  
+// console.log(Math.max(20, 10, 50, 40));  //50
+
+// 6-task
+// console.log(Math.random() * (4-2) + 2);  //4.304609675922521
+
+// 7-task
+// const message = "Welcome to Bahamas!";
+// console.log(message.length); //19
+
+// 8-task
+// const message = "Welcome to Bahamas!";
+// console.log(message.toUpperCase()); //WELCOME TO BAHAMAS!
+
+// 9-task
+// let user = {};
+// user.name = "Dasha";
+// user.age = "17";
+// user.city = "Kyiv";
+// console.log(user);  //{name: 'Dasha', age: '17', city: 'Kyiv'}
+// delete user.city;
+// user["like flowers"] = true;
+// console.log(user);  //{name: 'Dasha', age: '17', like flowers: true}
+
+// 10-task
+// for(key in user) {
+//     console.log(key)
+//     console.log(user[key]);
+// }
