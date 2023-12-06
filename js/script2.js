c435716751c9d77e0441f0a111ac5ca93875cce2
Ir200on <snezokdasha@gmail.com>
2023-12-06 04:55:34 +0200

    final 2 lesson

--- js/script2.js
+++ js/script2.js
@@ -62,8 +62,46 @@
 // }
 
 // 4-task
-
-// Зробіть сервіс який отримує число від 1 до 12
-// виведіть місяць який дорівнює числу
+// const month = prompt("Введіть порядковий номер місяцю від 1 до 12");
+// if (month > 0 && month < 2) {
+//   alert("Січень");
+// } else if (month > 1 && month < 3) {
+//   alert("Лютий");
+// } else if (month > 2 && month < 4) {
+//   alert("Березень");
+// } else if (month > 3 && month < 5) {
+//   alert("Квітень");
+// } else if (month > 4 && month < 6) {
+//   alert("Травень");
+// } else if (month > 5 && month < 7) {
+//   alert("Червень");
+// } else if (month > 6 && month < 8) {
+//   alert("Липень");
+// } else if (month > 7 && month < 9) {
+//   alert("Серпень");
+// } else if (month > 8 && month < 10) {
+//   alert("Вересень");
+// } else if (month > 9 && month < 11) {
+//   alert("Жовтень");
+// } else if (month > 10 && month < 12) {
+//   alert("Листопад");
+// } else if (month > 11 && month < 13) {
+//   alert("Грудень");
+// } else {
+//   alert("Введіть правильну цифру");
+// }
 
 // 5-task
+// let n = prompt("Введіть трьохзначне число");
+// if (n >= 100 && n < 1000) {
+//   let sum = 0;
+
+//   while (n > 0) {
+//     let ld = n % 10;
+//     sum += ld;
+//     n = Math.floor(n / 10);
+//   }
+//   alert(sum);
+// } else {
+//   alert("Щось пішло не так");
+// }
