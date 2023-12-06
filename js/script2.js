06174803043b287e909b48097836e6b6272255d9
Ir200on <snezokdasha@gmail.com>
2023-12-06 04:17:48 +0200

    2 homework

---
+++ js/script2.js
@@ -0,0 +1,69 @@
+// 1-task
+// let a = 1;
+// let b = 0;
+// let c = -3;
+
+// if (a > b) {
+//     console.log(true);  //true
+// } else if (a < b) {
+//     console.log(false);
+// }
+
+// if (b > c) {
+//     console.log(true);  //true
+// } else if (b < c) {
+//     console.log(false);
+// }
+
+// if (c > b) {
+//     console.log(true);  //false
+// } else if (c < b) {
+//     console.log(false);
+// }
+
+// if (b > a) {
+//     console.log(true);  //false
+// } else if (b < a) {
+//     console.log(false);
+// }
+
+// 2-task
+// let a = "test";
+// let b = "qwerty";
+// let c = "true";
+
+// if (a === "test") {
+//     console.log(true); //true
+// } else {
+//     console.log(false);
+// }
+
+// 3-task
+// let a = 1;
+// let b = 10;
+// let c = 13;
+
+// if (a > 10) {
+//     console.log(a - 5);
+// } else if (a < 10) {
+//     console.log(a + 5); //6
+// }
+
+// if (b >= 10) {
+//     console.log(b - 5); //5
+// } else if (b <= 10) {
+//     console.log(b + 5);
+// }
+
+// if (c > 10) {
+//     console.log(c - 5); //8
+// } else if (c < 10) {
+//     console.log(c + 5);
+// }
+
+// 4-task
+
+// Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// 5-task
