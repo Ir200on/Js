e839b7017a5cab9911b04c2d91316210f0b03868
Ir200on <snezokdasha@gmail.com>
2023-12-10 08:26:15 +0200

    6lessson.js

---
+++ js/script6.js
@@ -0,0 +1,31 @@
+// 1-task
+// class Abonent {
+//     constructor({ name }) {
+//     this.firstName = "";
+//     this.number = "";
+//     this.name = name;
+//     }
+//     set name(newName) {
+//     const nameRow = newName.split(" ");
+//     this.firstName = nameRow[0];
+//     this.number = nameRow[1];
+//     }
+//     get name() {
+//     return `firstName: ${this.firstName} number: ${this.number}`;
+//     }
+// }
+    
+// const Vlad = new Abonent({
+//     name: "Vlad 099-982-09-09",
+// });
+    
+// const Oksana = new Abonent({
+//     name: "Oksana 099-982-09-08",
+// });
+// const Vitalina = new Abonent({
+//     name: "Vitalina 099-982-09-07",
+// });
+    
+// console.log(Vlad.name);
+// console.log(Oksana.name);
+// console.log(Vitalina.name);
