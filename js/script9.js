0b821a237307aa2d17272eb5eeb44ae4314154c6
Ir200on <snezokdasha@gmail.com>
2023-12-20 09:01:35 +0200

    js9

--- js/script9.js
+++ js/script9.js
@@ -1,12 +1,17 @@
 // 1-task
 // Напишіть такий JavaScript, щоб після натискання на кнопку button, елемент
 // <div id="text"> зникав.
-
+// const button = document.querySelector('button');
+// const div = document.querySelector('#text');
+// button.addEventListener('click', () => div.style.display = 'none');
 
 // 2-task
 // Напишіть такий код, щоб після натискання на кнопку, вона зникала.
-
+// const button = document.querySelector('button');
+// button.addEventListener('click', () => button.style.display = 'none');
 
 // 3-task
 // Створіть дерево, яке показує/приховує дочірні вузли при кліц
+// const treeItems = document.querySelectorAll('li');
+// treeItems.forEach(item => item.addEventListener('click', () => item.classList.toggle('collapsed')));
 
